05ed2269ae7ebb6866e7e748e394424df4fafd6a
Pranjali Khairnar <106724039+praanjal2131@users.noreply.github.com>
2022-06-30 16:38:27 +0530

    Add files via upload

---
+++ Pattern7.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Pattern7
+{
+	public static void main(String[] args)
+	{
+	
+	for(int i=5;i>=1;i--)
+	{
+		//System.out.print("\t"+i);
+		
+		for(int j=5;j>=i;j--)
+		{
+			
+			System.out.print(j + " ");
+			
+			
+		}
+		  System.out.println();     
+	
+	}
+	
+	}
+}
