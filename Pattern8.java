056666c88bd17efab61d8ee1a117af0650128bcf
Pranjali Khairnar <106724039+praanjal2131@users.noreply.github.com>
2022-06-30 16:54:59 +0530

    Add files via upload

---
+++ Pattern8.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Pattern8
+{
+	public static void main(String[] args)
+	{
+	
+	for(int i=5;i>=1;i--)
+	{
+		//System.out.print("\t"+i);
+		
+		for(int j=i;j>=1;j--)
+		{
+			
+			System.out.print(j + " ");
+			
+			
+		}
+		  System.out.println();     
+	
+	}
+	
+	}
+}
